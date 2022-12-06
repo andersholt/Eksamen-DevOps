581fa46b9c42d6fce3eac81e18500383db36c742
EC2 Default User <ec2-user@ip-172-31-47-15.eu-west-1.compute.internal>
2022-12-06 22:10:21 +0000

    testing edit

--- infra/dashboards.tf
+++ infra/dashboards.tf
@@ -21,44 +21,6 @@ resource "aws_cloudwatch_dashboard" "main" {
         "region": "eu-west-1",
         "title": "Amount of carts"
       }
-    },
-  {
-      "type": "metric",
-      "x": 0,
-      "y": 0,
-      "width": 12,
-      "height": 6,
-      "properties": {
-        "metrics": [
-          [
-            "${var.candidate_id}",
-            "checkouts.value"
-          ]
-        ],
-        "period": 300,
-        "stat": "Maximum",
-        "region": "eu-west-1",
-        "title": "Amount of carts"
-      }
-    },
-  {
-      "type": "metric",
-      "x": 0,
-      "y": 0,
-      "width": 12,
-      "height": 6,
-      "properties": {
-        "metrics": [
-          [
-            "${var.candidate_id}",
-            "cartsvalue.value"
-          ]
-        ],
-        "period": 300,
-        "stat": "Maximum",
-        "region": "eu-west-1",
-        "title": "Amount of carts"
-      }
     }
   ]
 }
